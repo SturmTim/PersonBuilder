ff1b62d4942a6280a564eba1357aff188e33b031
SturmTim <71697727+SturmTim@users.noreply.github.com>
2022-12-15 16:31:56 +0100

    Finished

--- PersonBuilder/Program.cs
+++ PersonBuilder/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PersonBuilder;
+
+File.ReadLines("195a_persons_with_address.csv")
+    .Skip(1)
+    .Select(x => x.Split(";"))
+    .ToList()
+    .ForEach(x =>
+    {
+        var builder = new Person.Builder(x[0], x[1]);
+        if(x[2].Length > 0)
+        {
+            builder.SetAge(Int32.Parse(x[2]));
+        }
+
+        if (x[3].Length > 0)
+        {
+            builder.SetPhone(x[3]);
+        }
+
+        if (x[4].Length > 0)
+        {
+            builder.SetAddress(x[4]);
+        }
+
+        builder.Build();
+    });
